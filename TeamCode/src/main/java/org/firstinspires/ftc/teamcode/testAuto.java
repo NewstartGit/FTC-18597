abb6f3bf0abdafe189c9cdce8950db4eafa9cb9b
NewstartGit <SamuelRojas2066@gmail.com>
2023-04-19 01:09:51 -0400

    first testAuto, hoping to update the repository belonging to me so that the laptop can access this code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testAuto.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="testAuto", group="Linear Opmode")
+public class testAuto extends LinearOpMode {
+
+    private DcMotor testMotor = null;
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        testMotor = hardwareMap.get(DcMotor.class, "testMotor");
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            testMotor.setPower(1);
+            Thread.sleep(5000);
+        }
+    }
+}
